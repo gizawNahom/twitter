abcdc534089b6e558d37210b259fb63c90c894bf
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-12-06 11:56:38 +0300

    refactor: add space

--- apps/server/src/adapter-persistance-inMemory/InMemoryPostRepository.ts
+++ apps/server/src/adapter-persistance-inMemory/InMemoryPostRepository.ts
@@ -3,14 +3,17 @@ import { PostRepository } from '../core/postRepository';
 
 export class InMemoryPostRepository implements PostRepository {
   private posts: Array<Post> = [];
+
   async getAll(userId: string): Promise<Array<Post> | null> {
     return this.posts.filter((p) => {
       return p.getId() !== userId;
     }) as Post[];
   }
+
   async getById(postId: string): Promise<Post | null> {
     return this.posts.find((p) => p.getId() === postId) as Post;
   }
+
   async save(post: Post) {
     post.setCreatedAt(new Date());
     this.posts.push(post);
