f9a86112d41136e1637e502bc45ff2db776f7f89
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-01 18:24:47 +0300

    test: remove duplications

--- apps/client/__tests__/pact.test.ts
+++ apps/client/__tests__/pact.test.ts
@@ -5,10 +5,22 @@ import { AnyTemplate, like } from '@pact-foundation/pact/src/dsl/matchers';
 import { createPostResponse } from '../mocks/values';
 import { resetClientStore, setCLient } from './utilities';
 import { fetchPost } from '../lib/redux/slices/postSlice/fetchPost';
+import { Post } from '../lib/redux/slices/postSlice/post';
 
 const baseUrl = new URL(process.env.NEXT_PUBLIC_API_BASE_URL as string);
 const PORT = +baseUrl.port;
 const PATH_NAME = baseUrl.pathname;
+
+const ERROR_MESSAGE = 'Server error';
+
+function assertPostEquality(post: Post | null) {
+  expect(post).toEqual({
+    id: createPostResponse.id,
+    text: createPostResponse.text,
+    userId: createPostResponse.userId,
+    createdAt: new Date(createPostResponse.createdAt),
+  });
+}
 
 const provider = new Pact({
   dir: path.resolve(process.cwd(), 'pacts'),
@@ -23,17 +35,17 @@ afterEach(async () => await provider.verify());
 
 afterAll(async () => await provider.finalize());
 
+beforeAll(() => {
+  setCLient();
+});
+
+afterEach(async () => {
+  await resetClientStore();
+});
+
 describe('Create Post', () => {
-  beforeAll(() => {
-    setCLient();
-  });
-
-  afterEach(async () => {
-    await resetClientStore();
-  });
-
   test('creates post', async () => {
-    await addInteractionWithResponseBody({
+    await addInteractionWithBody({
       data: {
         createPost: {
           id: like(createPostResponse.id),
@@ -48,21 +60,15 @@ describe('Create Post', () => {
     const [post, errors] = await createPost(createPostResponse.text);
 
     expect(errors).toBe(null);
-    expect(post).toEqual({
-      id: createPostResponse.id,
-      text: createPostResponse.text,
-      userId: createPostResponse.userId,
-      createdAt: new Date(createPostResponse.createdAt),
-    });
+    assertPostEquality(post);
   });
 
   test('handles error', async () => {
-    const errorMessage = 'Server error';
-    await addInteractionWithResponseBody({
+    await addInteractionWithBody({
       data: null,
       errors: [
         {
-          message: errorMessage,
+          message: ERROR_MESSAGE,
         },
       ],
     });
@@ -71,13 +77,13 @@ describe('Create Post', () => {
 
     expect(errors).not.toBe(null);
     expect(errors?.length).toBe(1);
-    expect((errors as string[])[0]).toBe(errorMessage);
+    expect((errors as string[])[0]).toBe(ERROR_MESSAGE);
     expect(post).toBe(null);
   });
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async function addInteractionWithResponseBody(responseBody: any) {
-    const interaction = new GraphQLInteraction()
+  async function addInteractionWithBody(responseBody: any) {
+    const interaction = createBaseInteraction(responseBody)
       .uponReceiving('a request to create a post')
       .withOperation('createPost')
       .withMutation(
@@ -93,31 +99,12 @@ describe('Create Post', () => {
       )
       .withVariables({
         text: like(createPostResponse.text),
-      })
-      .withRequest({
-        path: PATH_NAME,
-        method: 'POST',
-      })
-      .willRespondWith({
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json; charset=utf-8',
-        },
-        body: responseBody,
       });
     await provider.addInteraction(interaction);
   }
 });
 
 describe('Fetch Post', () => {
-  beforeAll(() => {
-    setCLient();
-  });
-
-  afterEach(async () => {
-    await resetClientStore();
-  });
-
   test('fetches post', async () => {
     await addInteractionWithBody({
       data: {
@@ -133,21 +120,15 @@ describe('Fetch Post', () => {
 
     const post = await fetchPost(createPostResponse.id);
 
-    expect(post).toEqual({
-      id: createPostResponse.id,
-      text: createPostResponse.text,
-      userId: createPostResponse.userId,
-      createdAt: new Date(createPostResponse.createdAt),
-    });
+    assertPostEquality(post);
   });
 
   test('handles error', async () => {
-    const errorMessage = 'Server error';
     await addInteractionWithBody({
       data: null,
       errors: [
         {
-          message: errorMessage,
+          message: ERROR_MESSAGE,
         },
       ],
     });
@@ -156,26 +137,32 @@ describe('Fetch Post', () => {
       await fetchPost(createPostResponse.id);
     }).rejects.toThrow();
   });
+
+  async function addInteractionWithBody(responseBody: AnyTemplate) {
+    const interaction = createBaseInteraction(responseBody)
+      .uponReceiving('a request to fetch a post')
+      .withOperation('post')
+      .withMutation(
+        `query post($id: ID!) {
+            post(id: $id) {
+              id
+              text
+              userId
+              createdAt
+              __typename
+            }
+          }`
+      )
+      .withVariables({
+        id: like(createPostResponse.id),
+      });
+    await provider.addInteraction(interaction);
+  }
 });
 
-async function addInteractionWithBody(responseBody: AnyTemplate) {
-  const interaction = new GraphQLInteraction()
-    .uponReceiving('a request to fetch a post')
-    .withOperation('post')
-    .withMutation(
-      `query post($id: ID!) {
-          post(id: $id) {
-            id
-            text
-            userId
-            createdAt
-            __typename
-          }
-        }`
-    )
-    .withVariables({
-      id: like(createPostResponse.id),
-    })
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function createBaseInteraction(responseBody: any) {
+  return new GraphQLInteraction()
     .withRequest({
       path: PATH_NAME,
       method: 'POST',
@@ -187,5 +174,4 @@ async function addInteractionWithBody(responseBody: AnyTemplate) {
       },
       body: responseBody,
     });
-  await provider.addInteraction(interaction);
 }
