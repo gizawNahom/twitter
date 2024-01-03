f493ee330621854a9654fb9d0991d72a7205dbc5
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-03 15:50:26 +0300

    new: add style to status page

--- apps/client/pages/[userId]/status/[id].tsx
+++ apps/client/pages/[userId]/status/[id].tsx
@@ -22,19 +22,63 @@ export default function Status() {
   }, [id, dispatch]);
 
   return (
-    <div>
-      <button aria-label="back" onClick={() => router.back()}>
-        icon
-      </button>
-      <h1>Post</h1>
+    <div className="px-3">
+      <div className="flex justify-between gap-x-8 items-center pt-2">
+        <button
+          aria-label="back"
+          onClick={() => router.back()}
+          className="h-5 w-5 -ml-[0.2rem] p-0 relative inline-block before:block before:absolute before:hover:bg-slate-200 before:active:bg-slate-300 before:w-full before:h-full before:-z-10 before:rounded-full before:p-4 before:-top-[6px] before:-left-[6px]"
+        >
+          <svg
+            width="20px"
+            height="20px"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#000000"
+              d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+            />
+            <path
+              fill="#000000"
+              d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+            />
+          </svg>
+        </button>
+        <h1 className="grow text-xl font-medium">Post</h1>
+      </div>
       {fetchedPost && (
-        <div>
+        <div className="h-min flex flex-col gap-y-3 pt-3">
+          <div className="flex gap-3 items-center">
+            <div className="bg-slate-300 h-10 w-10 rounded-full border "></div>
+            <div>
+              <p className="font-bold text-lg">John Doe</p>
+              <p className="text-slate-500 -mt-1">@john</p>
+            </div>
+          </div>
           <p>{fetchedPost.text}</p>
-          <p>{formatDate(fetchedPost.createdAt)}</p>
+          <p className="text-slate-500 text-sm">
+            {formatDate(fetchedPost.createdAt)}
+          </p>
         </div>
       )}
-      {fetchStatus === 'failed' && 'Something went wrong'}
-      {fetchStatus === 'loading' && 'loading'}
+      <div className="min-h-[150px] flex justify-center items-center">
+        {fetchStatus === 'loading' && (
+          <div
+            className={
+              'border-cyan-500 align-middle inline-block h-5 w-5 animate-spin rounded-full border-[3px] border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]'
+            }
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        )}
+        {fetchStatus === 'failed' && (
+          <p className="text-slate-500">Something went wrong</p>
+        )}
+      </div>
     </div>
   );
 
