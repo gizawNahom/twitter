7a4b2f97665c8cfb3833e90b7375319c98a05147
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-08 18:29:19 +0300

    new: style  nav component

--- apps/client/__tests__/components/nav.test.tsx
+++ apps/client/__tests__/components/nav.test.tsx
@@ -2,10 +2,16 @@ import { screen } from '@testing-library/react';
 import { Nav } from '../../components/nav';
 import { renderElement } from '../utilities';
 
-const HOME_TEXT = /Home/i;
+const HOME_Link = '/home';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockImplementation(() => ({
+    pathname: '/path',
+  })),
+}));
 
 test('displays links', async () => {
   renderElement(<Nav />);
 
-  expect(screen.queryByText(HOME_TEXT)).toHaveAttribute('href', '/home');
+  expect(screen.queryByRole('link', { name: HOME_Link }));
 });
--- apps/client/components/nav.tsx
+++ apps/client/components/nav.tsx
@@ -1,13 +1,35 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Nav() {
+  const router = useRouter();
+
   return (
-    <nav className=" basis-1/4 flex flex-col pl-14 pt-3">
+    <nav className=" basis-1/4 flex flex-col items-center pl-14 pt-14">
       <ul>
-        <li className=" text-xl">
-          <Link href="/home">Home</Link>
-        </li>
+        <Link href="/home">
+          <li className=" text-xl flex gap-4 hover:bg-slate-200 rounded-full py-2 px-4 transition">
+            <svg
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              width="30px"
+              height="30px"
+              className={`stroke-black stroke-1 ${
+                isHome() ? 'fill-black' : 'fill-white'
+              }`}
+            >
+              <g>
+                <path d="M21.591 7.146L12.52 1.157c-.316-.21-.724-.21-1.04 0l-9.071 5.99c-.26.173-.409.456-.409.757v13.183c0 .502.418.913.929.913H9.14c.51 0 .929-.41.929-.913v-7.075h3.909v7.075c0 .502.417.913.928.913h6.165c.511 0 .929-.41.929-.913V7.904c0-.301-.158-.584-.408-.758z"></path>
+              </g>
+            </svg>
+            <span className={isHome() ? 'font-bold' : ''}>Home</span>
+          </li>
+        </Link>
       </ul>
     </nav>
   );
+
+  function isHome() {
+    return router.pathname === '/home';
+  }
 }
