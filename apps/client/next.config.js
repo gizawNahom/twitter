1149269597a6a9568908270d4a757245a5f361df
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-09 13:11:05 +0300

    new: add permanent redirect from '/' to '/home'

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const plugins = [
