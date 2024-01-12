6a3a510935ce14b0d7b628ca49bb3e6725cfb408
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-12 18:50:31 +0300

    refactor: use tailwind to style back svg

--- apps/client/components/backButton.tsx
+++ apps/client/components/backButton.tsx
@@ -14,19 +14,12 @@ export function BackButton() {
         className="h-5 w-5 -ml-[0.2rem] p-0 relative inline-block before:block before:absolute before:hover:bg-slate-200 before:active:bg-slate-300 before:w-full before:h-full before:-z-10 before:rounded-full before:p-4 before:-top-[6px] before:-left-[6px]"
       >
         <svg
-          width="20px"
-          height="20px"
           viewBox="0 0 1024 1024"
           xmlns="http://www.w3.org/2000/svg"
+          className=" w-5 h-5 fill-black"
         >
-          <path
-            fill="#000000"
-            d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
-          />
-          <path
-            fill="#000000"
-            d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
-          />
+          <path d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z" />
+          <path d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z" />
         </svg>
       </button>
     </div>
