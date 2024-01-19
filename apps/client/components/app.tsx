4edc32add33911ef64f080ea42bbd23b06bf5cf0
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-19 11:33:22 +0300

    refactor: move PostFAB to App

--- apps/client/components/app.tsx
+++ apps/client/components/app.tsx
@@ -1,11 +1,15 @@
 import { ReactNode } from 'react';
 import { Nav } from './nav';
+import { PostFAB } from './postFAB';
 
 export function App({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <Nav></Nav>
       <div className="grow lg:basis-1/2">{children}</div>
+      <div className="sm:hidden absolute bottom-16 right-5">
+        <PostFAB />
+      </div>
     </div>
   );
 }
--- apps/client/pages/home/index.tsx
+++ apps/client/pages/home/index.tsx
@@ -1,15 +1,10 @@
 import { PostForm } from '../../components/postForm';
-import { PostFAB } from '../../components/postFAB';
 
 export default function Index() {
   return (
     <div>
       <div className="hidden sm:block">
         <PostForm />
-      </div>
-
-      <div className="sm:hidden absolute bottom-16 right-5">
-        <PostFAB />
       </div>
     </div>
   );
