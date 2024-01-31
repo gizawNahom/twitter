89b1765385980ef1d0917ea5e3b36c52e8de2a30
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-01-31 17:06:59 +0300

    fix: remove misleading log

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,6 +2,6 @@ import { app } from './app';
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
