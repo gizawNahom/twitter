f55ee4d50c5464006204bbbbd2b09229cbe71e23
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-02-15 09:34:34 +0300

    new: make content padding optional in Page component

--- apps/client/components/page.tsx
+++ apps/client/components/page.tsx
@@ -4,17 +4,19 @@ import { BackButton } from './backButton';
 export function Page({
   children,
   title,
+  padded = true,
 }: {
   children: ReactNode;
   title?: ReactNode;
+  padded?: boolean;
 }) {
   return (
-    <div className="px-4">
-      <div className="flex justify-between gap-x-8 items-center pt-2">
+    <div>
+      <div className="flex justify-between gap-x-8 items-center pt-2 px-4">
         <BackButton />
         {title}
       </div>
-      {children}
+      <div className={padded ? 'px-4' : ''}>{children}</div>
     </div>
   );
 }
--- apps/client/pages/username/index.tsx
+++ apps/client/pages/username/index.tsx
@@ -4,8 +4,10 @@ import { Page } from '../../components/page';
 export default function Profile() {
   return (
     <div>
-      <Page>
-        <h4>Posts</h4>
+      <Page padded={false}>
+        <div className=" border-b-[1px]">
+          <h4>Posts</h4>
+        </div>
         <Posts userId="userId1" />
       </Page>
     </div>
