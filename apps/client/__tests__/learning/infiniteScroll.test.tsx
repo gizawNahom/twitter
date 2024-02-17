a1de02ce0966cf806f1f0434adb399744aa189e3
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-02-17 17:05:53 +0300

    test: add InfiniteScroller learning test

---
+++ apps/client/__tests__/learning/infiniteScroll.test.tsx
@@ -0,0 +1,47 @@
+import { screen, waitFor } from '@testing-library/react';
+import InfiniteScroll from 'react-infinite-scroller';
+import { renderElement } from '../utilities/helpers';
+import { useEffect, useState } from 'react';
+
+test('should render', async () => {
+  renderElement(<Component />);
+
+  await waitFor(() => {
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+  await waitFor(() => {
+    expect(screen.getAllByText(/item/i).length).toBeGreaterThan(1);
+  });
+});
+
+function Component() {
+  const [items, setItems] = useState<Array<unknown>>([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    fetchMoreData();
+  });
+
+  const fetchMoreData = () => {
+    if (items.length >= 500) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setItems([...items, ...Array.from({ length: 2 })]);
+    }, 900);
+  };
+
+  return (
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={fetchMoreData}
+      hasMore={hasMore}
+      loader={<h1 key="loader">Loading</h1>}
+    >
+      {items.map((e, i) => (
+        <div key={i}>Item {i}</div>
+      ))}
+    </InfiniteScroll>
+  );
+}
