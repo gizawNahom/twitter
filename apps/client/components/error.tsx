baab2f974c1a0ab754694086a1e0d23e58cf3387
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-02-22 17:37:22 +0300

    new: change Error component text

--- apps/client/components/error.tsx
+++ apps/client/components/error.tsx
@@ -1,7 +1,7 @@
 export function Error() {
   return (
     <div data-testid="error">
-      <p className="text-slate-500">Something went wrong</p>
+      <p className="text-slate-500">Something went wrong. Try reloading.</p>
     </div>
   );
 }
