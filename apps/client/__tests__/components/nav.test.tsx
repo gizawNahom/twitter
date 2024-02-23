62cabf6c95f75c3cdd5f2c56cdb38dd44c0da636
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-02-23 17:02:07 +0300

    new: add profile in nav

--- apps/client/__tests__/components/nav.test.tsx
+++ apps/client/__tests__/components/nav.test.tsx
@@ -4,6 +4,7 @@ import { renderElement } from '../testUtilities/helpers';
 import { POST_FAB_TEST_ID } from '../testUtilities/testIds';
 
 const HOME_LINK = '/home';
+const PROFILE_LINK = '/home';
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn().mockImplementation(() => ({
@@ -15,5 +16,6 @@ test('initial', async () => {
   renderElement(<Nav />);
 
   expect(screen.queryByRole('link', { name: HOME_LINK }));
+  expect(screen.queryByRole('link', { name: PROFILE_LINK }));
   expect(screen.getByTestId(POST_FAB_TEST_ID)).toBeInTheDocument();
 });
--- apps/client/components/nav.tsx
+++ apps/client/components/nav.tsx
@@ -10,7 +10,7 @@ export function Nav() {
       data-testid="nav"
       className="sm:border-r-[1px] sm:basis-1/6 sm:flex sm:flex-col sm:items-center sm:pt-14 sm:h-screen md:items-end md:pr-5 xl:basis-1/4 xl:pl-12"
     >
-      <ul className="absolute bottom-0 left-0 w-full flex justify-center border-t-[1px] sm:static sm:w-min sm:border-none xl:w-full">
+      <ul className="absolute bottom-0 left-0 w-full flex justify-around border-t-[1px] sm:flex-col sm:static sm:w-min sm:border-none xl:w-full">
         <Link href="/home" aria-label="Home">
           <li className="text-xl flex gap-4 hover:bg-slate-200 rounded-full p-3 transition lg:w-min">
             <svg
@@ -33,6 +33,28 @@ export function Nav() {
             </span>
           </li>
         </Link>
+        <Link href="/username" aria-label="Home">
+          <li className="text-xl flex gap-4 hover:bg-slate-200 rounded-full p-3 transition lg:w-min">
+            <svg
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+              className={`stroke-black stroke-1 w-7 h-7 ${
+                isProfile() ? 'fill-black' : 'fill-white stroke-2'
+              }`}
+            >
+              <g>
+                <path d="M17.863 13.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H3.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46C7.627 11.85 9.648 11 12 11s4.373.85 5.863 2.44zM12 2C9.791 2 8 3.79 8 6s1.791 4 4 4 4-1.79 4-4-1.791-4-4-4z"></path>
+              </g>
+            </svg>
+            <span
+              className={`${
+                isProfile() ? 'font-bold' : ''
+              } hidden xl:inline-block`}
+            >
+              Profile
+            </span>
+          </li>
+        </Link>
       </ul>
       <div className="absolute bottom-20 right-5 sm:static xl:hidden">
         <PostFAB />
@@ -42,5 +64,9 @@ export function Nav() {
 
   function isHome() {
     return router.pathname === '/home';
+  }
+
+  function isProfile() {
+    return router.pathname === '/username';
   }
 }
