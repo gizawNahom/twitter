53cdacd1fed079b3fb7c3693fa5029a69fca3439
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-02-29 19:49:21 +0300

    new: make ComposeTweet redirect to Home on successful post creation

--- apps/client/__tests__/pages/composeTweet.test.tsx
+++ apps/client/__tests__/pages/composeTweet.test.tsx
@@ -1,11 +1,21 @@
-import { screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import ComposeTweet from '../../pages/compose/tweet';
-import { renderElement } from '../testUtilities/helpers';
+import {
+  clickPostButton,
+  renderElement,
+  setUpApi,
+  typeText,
+} from '../testUtilities/helpers';
 import { POST_INPUT_PLACE_HOLDER_TEXT } from '../testUtilities/texts';
 import {
   BACK_BUTTON_TEST_ID,
   POST_FORM_TEST_ID,
 } from '../testUtilities/testIds';
+import { useRouter } from 'next/router';
+import { createPostAsync } from '../../lib/redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { reducer } from '../../lib/redux/rootReducer';
+import { Provider } from 'react-redux';
 
 jest.mock('next/router', () => ({
   useRouter: jest.fn(),
@@ -15,17 +25,62 @@ function renderSUT() {
   renderElement(<ComposeTweet />);
 }
 
-test('renders back button', () => {
+const push = jest.fn();
+
+function mockRouter() {
+  const router = useRouter as jest.Mock;
+  router.mockImplementation(() => ({
+    push: push,
+  }));
+}
+
+setUpApi();
+
+beforeEach(() => {
+  mockRouter();
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+test('initial', () => {
   renderSUT();
 
   expect(screen.queryByTestId(BACK_BUTTON_TEST_ID)).toBeVisible();
-});
-
-test('renders post input with input focused', () => {
-  renderSUT();
-
   expect(screen.queryByTestId(POST_FORM_TEST_ID)).toBeVisible();
   expect(
     screen.getByPlaceholderText(POST_INPUT_PLACE_HOLDER_TEXT)
   ).toHaveFocus();
+  expect(push).toHaveBeenCalledTimes(0);
+});
+
+test('redirects to home on successful post creation', async () => {
+  renderSUT();
+
+  await typeText('hello world');
+  await clickPostButton();
+
+  await waitFor(() => expect(push).toHaveBeenCalledTimes(1));
+  expect(push).toHaveBeenCalledWith('/home');
+});
+
+test('does not redirect to home if post creation was already successful', async () => {
+  const store = configureStore({
+    reducer,
+    middleware: (getDefaultMiddleware) => {
+      return getDefaultMiddleware({
+        serializableCheck: false,
+      });
+    },
+  });
+  await store.dispatch(createPostAsync('Hello'));
+
+  render(
+    <Provider store={store}>
+      <ComposeTweet />
+    </Provider>
+  );
+
+  expect(push).toHaveBeenCalledTimes(0);
 });
--- apps/client/pages/compose/tweet.tsx
+++ apps/client/pages/compose/tweet.tsx
@@ -1,7 +1,31 @@
 import { PostForm } from '../../components/postForm';
 import { Page } from '../../components/page';
+import { useEffect, useState } from 'react';
+import { selectCreateStatus, useSelector } from '../../lib/redux';
+import { useRouter } from 'next/router';
 
 export default function ComposeTweet() {
+  const router = useRouter();
+  const currentCreateStatus = useSelector(selectCreateStatus);
+  const [previousCreateStatus, setPreviousStatus] =
+    useState(currentCreateStatus);
+
+  useEffect(() => {
+    if (shouldGoToHome()) goToHome();
+    setPreviousStatus(currentCreateStatus);
+
+    function shouldGoToHome() {
+      return (
+        previousCreateStatus !== 'succeeded' &&
+        currentCreateStatus === 'succeeded'
+      );
+    }
+
+    function goToHome() {
+      router.push('/home');
+    }
+  }, [currentCreateStatus, router, previousCreateStatus]);
+
   return (
     <Page>
       <PostForm autoFocus={true} />
