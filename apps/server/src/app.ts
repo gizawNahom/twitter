c40c7412d0fdc416b7ea0149567e63947e2e4dd9
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-03-21 16:21:45 +0300

    new: make request parameters required

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -17,11 +17,11 @@ const typeDefs = `#graphql
   type Query {
     hello: String
     post(id: ID!): Post
-    posts(userId: ID!, limit: Int, offset: Int): [Post]
-    searchPosts(query: String!, limit: Int, offset: Int): [Post]
+    posts(userId: ID!, limit: Int!, offset: Int!): [Post]
+    searchPosts(query: String!, limit: Int!, offset: Int!): [Post]
   }
   type Mutation {
-    createPost(text: String): Post
+    createPost(text: String!): Post
   }
   type Post {
     id: ID!
