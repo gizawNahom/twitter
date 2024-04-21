fdfeddd0fff6729a9a43cfaa3a60ef32f75ce894
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-04-21 18:57:55 +0300

    new: fix width change when clear text appears

--- apps/client/components/searchInput.tsx
+++ apps/client/components/searchInput.tsx
@@ -60,23 +60,25 @@ export function SearchInput({
           ref={inputRef}
         />
       </div>
-      {!isEmpty(query) && (
-        <button
-          aria-label="clear text"
-          onClick={() => setQuery('')}
-          className=" bg-blue-500 rounded-full p-1"
-        >
-          <svg
-            viewBox="0 0 15 15"
-            aria-hidden="true"
-            className="w-2 h-2 transition fill-white sm:w-3 sm:h-3"
+      <div className="w-5 h-6">
+        {!isEmpty(query) && (
+          <button
+            aria-label="clear text"
+            onClick={() => setQuery('')}
+            className=" bg-blue-500 rounded-full p-1"
           >
-            <g>
-              <path d="M6.09 7.5L.04 1.46 1.46.04 7.5 6.09 13.54.04l1.42 1.42L8.91 7.5l6.05 6.04-1.42 1.42L7.5 8.91l-6.04 6.05-1.42-1.42L6.09 7.5z"></path>
-            </g>
-          </svg>
-        </button>
-      )}
+            <svg
+              viewBox="0 0 15 15"
+              aria-hidden="true"
+              className="w-2 h-2 transition fill-white sm:w-3 sm:h-3"
+            >
+              <g>
+                <path d="M6.09 7.5L.04 1.46 1.46.04 7.5 6.09 13.54.04l1.42 1.42L8.91 7.5l6.05 6.04-1.42 1.42L7.5 8.91l-6.04 6.05-1.42-1.42L6.09 7.5z"></path>
+              </g>
+            </svg>
+          </button>
+        )}
+      </div>
     </div>
   );
 
