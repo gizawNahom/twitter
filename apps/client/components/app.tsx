05b9c92c353262b1fa5dd9644fdae24bec64f7da
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-04-21 19:22:28 +0300

    new: style app for 2xl screen

--- apps/client/components/app.tsx
+++ apps/client/components/app.tsx
@@ -9,12 +9,14 @@ export function App({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <Nav></Nav>
-      <div className="sm:grow sm:max-w-xl sm:border-r-[1px] lg:min-w-[58%]">
+      <div className="sm:grow sm:max-w-xl sm:border-r-[1px] lg:min-w-[58%] xl:min-w-[45%]">
         {children}
       </div>
-      <div className="hidden grow justify-center items-start pt-1 lg:flex">
+      <div className="hidden grow items-start pt-1 lg:flex lg:px-5 xl:px-8">
         {router.pathname !== '/search' && (
-          <SearchInput onSubmit={pushToSearchPage} />
+          <div className="w-full">
+            <SearchInput onSubmit={pushToSearchPage} />
+          </div>
         )}
       </div>
     </div>
