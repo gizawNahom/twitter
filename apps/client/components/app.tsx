99e53b7fef42dfade112e24360fe564f85672904
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-04-21 18:32:36 +0300

    new: style search on xl screen

--- apps/client/components/app.tsx
+++ apps/client/components/app.tsx
@@ -9,11 +9,13 @@ export function App({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
       <Nav></Nav>
-      <div className="grow sm:max-w-xl sm:border-r-[1px]">{children}</div>
-      <div className="hidden lg:block">
-        <div className="hidden">
+      <div className="sm:grow sm:max-w-xl sm:border-r-[1px] lg:min-w-[58%]">
+        {children}
+      </div>
+      <div className="hidden grow justify-center items-start pt-1 lg:flex">
+        {router.pathname !== '/search' && (
           <SearchInput onSubmit={pushToSearchPage} />
-        </div>
+        )}
       </div>
     </div>
   );
