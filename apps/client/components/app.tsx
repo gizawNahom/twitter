64fd582118ea60b95bc7d6b5562f0ffb834bf221
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-04-21 22:28:33 +0300

    new: style app component on 3xl screen

--- apps/client/components/app.tsx
+++ apps/client/components/app.tsx
@@ -7,12 +7,12 @@ export function App({ children }: { children: ReactNode }) {
   const router = useRouter();
 
   return (
-    <div className="flex">
+    <div className="flex 2xl:justify-center">
       <Nav></Nav>
-      <div className="sm:grow sm:max-w-xl sm:border-r-[1px] lg:min-w-[58%] xl:min-w-[45%]">
+      <div className="sm:grow sm:max-w-xl sm:border-r-[1px] lg:max-w-[600px] lg:min-w-[600px]">
         {children}
       </div>
-      <div className="hidden grow items-start pt-1 lg:flex lg:px-5 xl:px-8">
+      <div className="hidden grow items-start pt-1 lg:flex lg:px-5 xl:px-8 xl:max-w-[412px] xl:min-w-[412px]">
         {router.pathname !== '/search' && (
           <div className="w-full">
             <SearchInput onSubmit={pushToSearchPage} />
