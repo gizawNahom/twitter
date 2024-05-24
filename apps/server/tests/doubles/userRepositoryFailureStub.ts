8d0cbb13c1a23244fa8c35e32d66c049e14d2543
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-05-24 16:20:25 +0300

    refactor: remove unused parameter

--- apps/server/tests/doubles/userRepositoryFailureStub.ts
+++ apps/server/tests/doubles/userRepositoryFailureStub.ts
@@ -1,9 +1,8 @@
 import { User } from '../../src/core/entities/user';
-import { Username } from '../../src/core/entities/username';
 import { UserRepository } from '../../src/core/ports/userRepository';
 
 export class UserRepositoryFailureStub implements UserRepository {
-  async getUserId(username: Username): Promise<string | null> {
+  async getUserId(): Promise<string | null> {
     return null;
   }
 
