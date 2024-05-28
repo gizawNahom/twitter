295e8c8a593b213148a32fb492940f9fd9670f2f
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-05-28 09:39:22 +0300

    new: add invalid limit check

--- apps/server/src/core/useCases/readMessagesUseCase.ts
+++ apps/server/src/core/useCases/readMessagesUseCase.ts
@@ -1,11 +1,14 @@
+import { Limit } from '../valueObjects/limit';
 import { Token } from '../valueObjects/token';
 
 export class ReadMessagesUseCase {
-  async execute({ tokenString }: ReadMessagesRequest) {
+  async execute({ tokenString, limitValue }: ReadMessagesRequest) {
     new Token(tokenString);
+    new Limit(limitValue);
   }
 }
 
 export class ReadMessagesRequest {
   tokenString: string;
+  limitValue: number;
 }
--- apps/server/tests/core/readMessagesUseCase.test.ts
+++ apps/server/tests/core/readMessagesUseCase.test.ts
@@ -1,14 +1,18 @@
 import { ReadMessagesUseCase } from '../../src/core/useCases/readMessagesUseCase';
-import { sampleUserToken } from '../utilities/samples';
-import { testWithInvalidToken } from '../utilities/tests';
+import { sampleLimit, sampleUserToken } from '../utilities/samples';
+import { testWithInvalidLimit, testWithInvalidToken } from '../utilities/tests';
 
 async function executeUseCase({
   tokenString = sampleUserToken,
+  limitValue = sampleLimit,
 }: {
   tokenString?: string;
+  limitValue?: number;
 }) {
   const uC = new ReadMessagesUseCase();
-  return await uC.execute({ tokenString });
+  return await uC.execute({ tokenString, limitValue });
 }
 
 testWithInvalidToken((token) => executeUseCase({ tokenString: token }));
+
+testWithInvalidLimit((limitValue) => executeUseCase({ limitValue }));
