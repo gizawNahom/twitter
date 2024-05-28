bd8203677da511fc15c9914318573c66b57f4e4f
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-05-28 09:59:34 +0300

    new: add invalid chat id check

--- apps/server/src/core/useCases/readMessagesUseCase.ts
+++ apps/server/src/core/useCases/readMessagesUseCase.ts
@@ -1,12 +1,19 @@
+import { ChatId } from '../valueObjects/chatId';
 import { Limit } from '../valueObjects/limit';
 import { Offset } from '../valueObjects/offset';
 import { Token } from '../valueObjects/token';
 
 export class ReadMessagesUseCase {
-  async execute({ tokenString, limitValue, offsetValue }: ReadMessagesRequest) {
+  async execute({
+    tokenString,
+    limitValue,
+    offsetValue,
+    chatIdString,
+  }: ReadMessagesRequest) {
     new Token(tokenString);
     new Limit(limitValue);
     new Offset(offsetValue);
+    new ChatId(chatIdString);
   }
 }
 
@@ -14,4 +21,5 @@ export class ReadMessagesRequest {
   tokenString: string;
   limitValue: number;
   offsetValue: number;
+  chatIdString: string;
 }
--- apps/server/tests/core/readMessagesUseCase.test.ts
+++ apps/server/tests/core/readMessagesUseCase.test.ts
@@ -1,4 +1,6 @@
 import { ReadMessagesUseCase } from '../../src/core/useCases/readMessagesUseCase';
+import { assertValidationErrorWithMessage } from '../utilities/assertions';
+import { ERROR_CHAT_ID_REQUIRED } from '../utilities/errorMessages';
 import {
   sampleLimit,
   sampleOffset,
@@ -10,17 +12,27 @@ import {
   testWithInvalidToken,
 } from '../utilities/tests';
 
+const emptyString = ' \n\t\r';
+const sampleChatId = 'chatId123';
+
 async function executeUseCase({
   tokenString = sampleUserToken,
   limitValue = sampleLimit,
   offsetValue = sampleOffset,
+  chatIdString = sampleChatId,
 }: {
   tokenString?: string;
   limitValue?: number;
   offsetValue?: number;
+  chatIdString?: string;
 }) {
   const uC = new ReadMessagesUseCase();
-  return await uC.execute({ tokenString, limitValue, offsetValue });
+  return await uC.execute({
+    tokenString,
+    limitValue,
+    offsetValue,
+    chatIdString,
+  });
 }
 
 testWithInvalidToken((token) => executeUseCase({ tokenString: token }));
@@ -28,3 +40,10 @@ testWithInvalidToken((token) => executeUseCase({ tokenString: token }));
 testWithInvalidLimit((limitValue) => executeUseCase({ limitValue }));
 
 testWithInvalidOffset((offsetValue) => executeUseCase({ offsetValue }));
+
+test('throws if the chat id is empty', () => {
+  assertValidationErrorWithMessage(
+    () => executeUseCase({ chatIdString: emptyString }),
+    ERROR_CHAT_ID_REQUIRED
+  );
+});
