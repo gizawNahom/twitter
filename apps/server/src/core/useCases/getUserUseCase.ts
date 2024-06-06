666f14a344516bcc0e4123afacad006cbf8282c4
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-06-06 07:25:18 +0300

    new: add invalid limit check

--- apps/server/src/core/useCases/getUserUseCase.ts
+++ apps/server/src/core/useCases/getUserUseCase.ts
@@ -1,11 +1,14 @@
+import { Limit } from '../valueObjects/limit';
 import { Token } from '../valueObjects/token';
 
 export class GetUserUseCase {
-  execute({ tokenString }: GetUserRequest) {
+  execute({ tokenString, limitValue }: GetUserRequest) {
     new Token(tokenString);
+    new Limit(limitValue);
   }
 }
 
 export interface GetUserRequest {
   tokenString: string;
+  limitValue: number;
 }
--- apps/server/tests/core/getUserUseCase.test.ts
+++ apps/server/tests/core/getUserUseCase.test.ts
@@ -1,14 +1,18 @@
 import { GetUserUseCase } from '../../src/core/useCases/getUserUseCase';
-import { sampleUserToken } from '../utilities/samples';
-import { testWithInvalidToken } from '../utilities/tests';
+import { sampleLimit, sampleUserToken } from '../utilities/samples';
+import { testWithInvalidLimit, testWithInvalidToken } from '../utilities/tests';
 
 async function executeUseCase({
   tokenString = sampleUserToken,
+  limitValue = sampleLimit,
 }: {
   tokenString?: string;
+  limitValue?: number;
 }) {
   const uC = new GetUserUseCase();
-  return await uC.execute({ tokenString });
+  return await uC.execute({ tokenString, limitValue });
 }
 
 testWithInvalidToken((tokenString) => executeUseCase({ tokenString }));
+
+testWithInvalidLimit((limitValue) => executeUseCase({ limitValue }));
