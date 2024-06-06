44ddbae8455eb8139f66235104e3e9e0a9e68bd1
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-06-06 07:40:34 +0300

    new: add invalid username check

--- apps/server/src/core/useCases/getUserUseCase.ts
+++ apps/server/src/core/useCases/getUserUseCase.ts
@@ -1,12 +1,19 @@
+import { Username } from '../entities/username';
 import { Limit } from '../valueObjects/limit';
 import { Offset } from '../valueObjects/offset';
 import { Token } from '../valueObjects/token';
 
 export class GetUserUseCase {
-  execute({ tokenString, limitValue, offsetValue }: GetUserRequest) {
+  execute({
+    tokenString,
+    limitValue,
+    offsetValue,
+    usernameString,
+  }: GetUserRequest) {
     new Token(tokenString);
     new Limit(limitValue);
     new Offset(offsetValue);
+    new Username(usernameString);
   }
 }
 
@@ -14,4 +21,5 @@ export interface GetUserRequest {
   tokenString: string;
   limitValue: number;
   offsetValue: number;
+  usernameString: string;
 }
--- apps/server/tests/core/getUserUseCase.test.ts
+++ apps/server/tests/core/getUserUseCase.test.ts
@@ -1,5 +1,8 @@
 import { GetUserUseCase } from '../../src/core/useCases/getUserUseCase';
+import { assertValidationErrorWithMessage } from '../utilities/assertions';
+import { ERROR_USERNAME_INVALID } from '../utilities/errorMessages';
 import {
+  emptyString,
   sampleLimit,
   sampleOffset,
   sampleUserToken,
@@ -10,17 +13,26 @@ import {
   testWithInvalidToken,
 } from '../utilities/tests';
 
+const sampleUsername = 'sampleUserName';
+
 async function executeUseCase({
   tokenString = sampleUserToken,
   limitValue = sampleLimit,
   offsetValue = sampleOffset,
+  usernameString = sampleUsername,
 }: {
   tokenString?: string;
   limitValue?: number;
   offsetValue?: number;
+  usernameString?: string;
 }) {
   const uC = new GetUserUseCase();
-  return await uC.execute({ tokenString, limitValue, offsetValue });
+  return await uC.execute({
+    tokenString,
+    limitValue,
+    offsetValue,
+    usernameString,
+  });
 }
 
 testWithInvalidToken((tokenString) => executeUseCase({ tokenString }));
@@ -28,3 +40,21 @@ testWithInvalidToken((tokenString) => executeUseCase({ tokenString }));
 testWithInvalidLimit((limitValue) => executeUseCase({ limitValue }));
 
 testWithInvalidOffset((offsetValue) => executeUseCase({ offsetValue }));
+
+testWithInvalidUsername((usernameString) => executeUseCase({ usernameString }));
+
+function testWithInvalidUsername(
+  useCaseExecution: (usernameString) => Promise<unknown>
+) {
+  describe('throws with invalid username error message', () => {
+    test.each([[emptyString], ['a'.repeat(4)], ['/?<.>'], ['a'.repeat(16)]])(
+      'when username is %s',
+      async (usernameString) => {
+        assertValidationErrorWithMessage(
+          () => useCaseExecution(usernameString),
+          ERROR_USERNAME_INVALID
+        );
+      }
+    );
+  });
+}
