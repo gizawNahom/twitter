f32149ebf8af7cc805290c3cdccefeadc9284ea2
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-06-06 07:28:45 +0300

    new: add invalid offset check

--- apps/server/src/core/useCases/getUserUseCase.ts
+++ apps/server/src/core/useCases/getUserUseCase.ts
@@ -1,14 +1,17 @@
 import { Limit } from '../valueObjects/limit';
+import { Offset } from '../valueObjects/offset';
 import { Token } from '../valueObjects/token';
 
 export class GetUserUseCase {
-  execute({ tokenString, limitValue }: GetUserRequest) {
+  execute({ tokenString, limitValue, offsetValue }: GetUserRequest) {
     new Token(tokenString);
     new Limit(limitValue);
+    new Offset(offsetValue);
   }
 }
 
 export interface GetUserRequest {
   tokenString: string;
   limitValue: number;
+  offsetValue: number;
 }
--- apps/server/tests/core/getUserUseCase.test.ts
+++ apps/server/tests/core/getUserUseCase.test.ts
@@ -1,18 +1,30 @@
 import { GetUserUseCase } from '../../src/core/useCases/getUserUseCase';
-import { sampleLimit, sampleUserToken } from '../utilities/samples';
-import { testWithInvalidLimit, testWithInvalidToken } from '../utilities/tests';
+import {
+  sampleLimit,
+  sampleOffset,
+  sampleUserToken,
+} from '../utilities/samples';
+import {
+  testWithInvalidLimit,
+  testWithInvalidOffset,
+  testWithInvalidToken,
+} from '../utilities/tests';
 
 async function executeUseCase({
   tokenString = sampleUserToken,
   limitValue = sampleLimit,
+  offsetValue = sampleOffset,
 }: {
   tokenString?: string;
   limitValue?: number;
+  offsetValue?: number;
 }) {
   const uC = new GetUserUseCase();
-  return await uC.execute({ tokenString, limitValue });
+  return await uC.execute({ tokenString, limitValue, offsetValue });
 }
 
 testWithInvalidToken((tokenString) => executeUseCase({ tokenString }));
 
 testWithInvalidLimit((limitValue) => executeUseCase({ limitValue }));
+
+testWithInvalidOffset((offsetValue) => executeUseCase({ offsetValue }));
