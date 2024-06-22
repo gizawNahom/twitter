776b3f1981e109b0fbd08534da7be13b8443e246
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-06-22 07:41:14 +0300

    refactor: remove makeSureUserIsAuthenticated service

--- apps/server/src/core/domainServices/getAuthenticatedUserOrThrow.ts
+++ apps/server/src/core/domainServices/getAuthenticatedUserOrThrow.ts
@@ -1,8 +1,10 @@
+import { User } from '../entities/user';
+import { ValidationError } from '../errors';
 import { GateKeeper } from '../ports/gateKeeper';
 import { Logger } from '../ports/logger';
+import { ValidationMessages } from '../validationMessages';
 import { Token } from '../valueObjects/token';
 import { extractUser } from './extractUser';
-import { makeSureUserIsAuthenticated } from './makeSureUserIsAuthenticated';
 
 export async function getAuthenticatedUserOrThrow(
   token: Token,
@@ -12,4 +14,16 @@ export async function getAuthenticatedUserOrThrow(
   const user = await extractUser(gateKeeper, logger, token);
   makeSureUserIsAuthenticated(user);
   return user;
+}
+
+function makeSureUserIsAuthenticated(user: User) {
+  if (!isUserAuthenticated(user)) throwInvalidUserError();
+}
+
+function isUserAuthenticated(user) {
+  return user != null;
+}
+
+function throwInvalidUserError() {
+  throw new ValidationError(ValidationMessages.INVALID_USER);
 }
--- apps/server/src/core/domainServices/index.ts
+++ apps/server/src/core/domainServices/index.ts
@@ -1,4 +1,3 @@
-export * from './makeSureUserIsAuthenticated';
 export * from './sanitizeXSSString';
 export * from './extractUser';
 export * from './getAuthenticatedUserOrThrow';
