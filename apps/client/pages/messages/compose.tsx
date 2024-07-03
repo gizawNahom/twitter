9e080e9dfc486026989fa6a5f04d03b90f31a973
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-07-03 09:31:02 +0300

    new: add style to UserSearchInput

--- apps/client/pages/messages/compose.tsx
+++ apps/client/pages/messages/compose.tsx
@@ -15,11 +15,13 @@ export default function ComposeMessage() {
 
   return (
     <Page header={renderHeader()}>
-      <UserSearchInput
-        onThrottledChange={onChange}
-        searchInputValue={searchInputValue}
-        setSearchInputValue={setSearchInputValue}
-      />
+      <div className=" pb-2 pt-4">
+        <UserSearchInput
+          onThrottledChange={onChange}
+          searchInputValue={searchInputValue}
+          setSearchInputValue={setSearchInputValue}
+        />
+      </div>
       {selectedUser && renderSelectedUser(selectedUser)}
       {status == 'loading' && renderProgressBar()}
       {status == 'success' && renderMatchingUsers(users)}
@@ -141,16 +143,33 @@ export function UserSearchInput({
   searchInputValue: string;
   setSearchInputValue: (value: string) => void;
 }) {
+  const [isFocused, setIsFocused] = useState(false);
   useChange(onChange, value);
 
   return (
     <div data-testid="people-search">
-      <input
-        type="text"
-        placeholder="Search people"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-      />
+      <div className=" flex gap-x-5 items-center pl-[0.20rem]">
+        <svg
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+          className={`w-5 h-5 transition ${
+            isFocused ? 'fill-cyan-500' : 'fill-slate-500'
+          }`}
+        >
+          <g>
+            <path d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+          </g>
+        </svg>
+        <input
+          type="text"
+          placeholder="Search people"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          className=" w-full outline-none"
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </div>
     </div>
   );
 
