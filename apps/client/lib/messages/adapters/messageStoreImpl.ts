34c9dcac8c1eb09666cc807ccc649579a4879dec
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-09-30 14:45:04 +0300

    refactor: rename apollo message store

--- apps/client/lib/messages/adapters/messageStoreImpl.ts
+++ apps/client/lib/messages/adapters/messageStoreImpl.ts
@@ -2,7 +2,7 @@ import { Message } from '../core/domain/message';
 import { MessageStore } from '../core/ports/messageStore';
 import { CustomEvent } from '../../shared/customEvent';
 import { Client } from '../../../utilities/client';
-import { ApolloMessagesUpdated } from '../data-source-apollo/apolloMessageStore';
+import { ApolloMessagesUpdated } from '../data-source-apollo/apolloMessagesUpdated';
 
 export class MessageStoreImpl implements MessageStore {
   constructor(private _messagesUpdated: CustomEvent<Message[], string>) {}
