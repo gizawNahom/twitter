849ab4f0897336a71a922dfc385170929805a2ad
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-10-21 16:33:53 +0300

    refactor: extract send message controller

--- apps/client/lib/messages/ui/hooks/useSendMessage.ts
+++ apps/client/lib/messages/ui/hooks/useSendMessage.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Message } from '../../core/domain/message';
 import { useAuth } from '../../../auth/authContext';
 import { Context } from '../../context';
+import { SendMessageUseCase } from '../../core/useCases/sendMessageUseCase';
 
 export function useSendMessage() {
   const [status, setStatus] = useState<'idle' | 'loading' | 'error'>('idle');
@@ -21,16 +22,33 @@ export function useSendMessage() {
   ): Promise<Message | null | undefined> {
     try {
       setStatus('loading');
-      const message = await Context.sendMessageUseCase.execute({
-        senderId,
-        text,
-        chatId,
-      });
+      const controller = new SendMessageController(Context.sendMessageUseCase);
+      const message = await controller.sendMessage({ senderId, text, chatId });
       setMessage(message);
       setStatus('idle');
       return message;
     } catch (error) {
       setStatus('error');
     }
+  }
+}
+
+class SendMessageController {
+  constructor(private sendMessageUseCase: SendMessageUseCase) {}
+
+  async sendMessage({
+    senderId,
+    text,
+    chatId,
+  }: {
+    senderId: string;
+    text: string;
+    chatId: string;
+  }): Promise<Message> {
+    return (await this.sendMessageUseCase.execute({
+      senderId,
+      text,
+      chatId,
+    })) as Message;
   }
 }
