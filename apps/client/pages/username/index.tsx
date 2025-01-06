69a1066fa1660857ff292793ee143fabdaac40a5
Nahom <154278634+gizawNahom@users.noreply.github.com>
2025-01-06 09:37:16 +0300

    fix: make profile page fit full size on medium screen

--- apps/client/pages/username/index.tsx
+++ apps/client/pages/username/index.tsx
@@ -4,13 +4,11 @@ import { fetchPosts } from '../../lib/redux/slices/postsSlice/fetchPosts';
 
 export default function Profile() {
   return (
-    <div>
-      <Page isPadded={false}>
-        <div className="p-2 border-b-[1px]">
-          <h4 className="font-bold text-base">Posts</h4>
-        </div>
-        <Posts fetchPosts={(offset) => fetchPosts('userId1', offset, 20)} />
-      </Page>
-    </div>
+    <Page isPadded={false}>
+      <div className="p-2 border-b-[1px]">
+        <h4 className="font-bold text-base">Posts</h4>
+      </div>
+      <Posts fetchPosts={(offset) => fetchPosts('userId1', offset, 20)} />
+    </Page>
   );
 }
